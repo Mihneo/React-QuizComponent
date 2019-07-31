750e92a7e50eead5490bd6b58892c3db8e1bdfda
Mihnea Voronca <mihnea.voronea@3pillarglobal.com>
2019-07-31 10:39:30 +0300

    Completed the module

---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class QuizQuestionButton extends Component {
+render(){
+    return(
+<button>{button_text}</button>
+        )
+    }
+}
+
+
+
+
+
+export default QuizQuestionButton
